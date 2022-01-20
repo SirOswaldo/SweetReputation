64a5d178c5399052f0f26b07529f7bb7ec917cb3
SirOswaldo <DeveloperOswaldo@gmail.com>
2022-01-20 11:25:32 -0300

    Initial commit

---
+++ src/main/java/org/kayteam/sweetreputation/SweetReputation.java
@@ -0,0 +1,17 @@
+package org.kayteam.sweetreputation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SweetReputation extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
